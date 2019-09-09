fa1d89993a9864039d10bdaeef8a6dcf25b7fa4d
andrebargas <andrebargas@gmail.com>
2019-09-09 07:31:13 -0300

    Adicionando v1 do projeto

--- haskell/project/file_reader.hs
+++ haskell/project/file_reader.hs
@@ -1,11 +1,43 @@
 import System.IO
-import Control.Monad
+-- import Control.Monad
+import qualified Data.Text
+import Data.List.Split
+import Data.Array
 
 
-readTxtFile :: [Char] -> IO [Char]
--- readTxtFile " " = return "Voce deve o nome de um arquivo"
-readTxtFile file_name = do
-        let list = []
-        handle <- openFile file_name ReadMode
-        contents <- hGetContents handle
-        return contents
+
+-- readTxtFile :: [Char] -> String
+-- readTxtFile " " = return "Voce deve inserir o nome de um arquivo"
+readFileLines file_name = do
+        contents <- readFile file_name
+        let lines_list = lines contents
+        let a = doWords lines_list
+        escreveArq (joinWordsList ( mdParser (head a)))
+
+doWords [] = []
+doWords list = words (head list): doWords (tail list)
+
+mdParser [] = []
+mdParser wordList = if funcao (head wordList) == head wordList
+                    then [head wordList]++  mdParser (tail wordList)
+                    else (funcao (head wordList): mdParser (tail wordList))++ ["</" ++ (tail (funcao (head wordList)))]
+
+-- Tags para titulo
+funcao "#" = "<h1>"
+funcao "##" = "<h2>"
+funcao "###" = "<h3>"
+funcao "####" = "<h4>"
+funcao "#####" = "<h5>"
+funcao "######" = "<h6>"
+-- Texto puro
+funcao word = word
+
+joinWordsList [] = " "
+joinWordsList list =
+              ((head list)++ " ")++ joinWordsList (tail list)
+
+
+escreveArq txt = do
+              arq <- openFile "out.html" WriteMode
+              hPutStrLn arq (txt)
+              hClose arq;
