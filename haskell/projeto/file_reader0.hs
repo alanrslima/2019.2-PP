eb19f9d6f1e2dc5892afa319100f3fb78e3325b1
andrebargas <andrebargas@gmail.com>
2019-09-09 07:30:14 -0300

    Adicionando v1 do projeto

--- haskell/projeto/file_reader0.hs
+++ haskell/projeto/file_reader0.hs
@@ -1,5 +1,5 @@
 import System.IO
-import Control.Monad
+-- import Control.Monad
 import qualified Data.Text
 import Data.List.Split
 import Data.Array
@@ -7,27 +7,37 @@ import Data.Array
 
 
 -- readTxtFile :: [Char] -> String
--- readTxtFile " " = return "Voce deve o nome de um arquivo"
+-- readTxtFile " " = return "Voce deve inserir o nome de um arquivo"
 readFileLines file_name = do
         contents <- readFile file_name
-        let h = lines contents
-        let a = head h
-        -- let b = splitOn " " line
-        let b = show a
-        return b
+        let lines_list = lines contents
+        let a = doWords lines_list
+        escreveArq (joinWordsList ( mdParser (head a)))
+
+doWords [] = []
+doWords list = words (head list): doWords (tail list)
+
+mdParser [] = []
+mdParser wordList = if funcao (head wordList) == head wordList
+                    then [head wordList]++  mdParser (tail wordList)
+                    else (funcao (head wordList): mdParser (tail wordList))++ ["</" ++ (tail (funcao (head wordList)))]
+
+-- Tags para titulo
+funcao "#" = "<h1>"
+funcao "##" = "<h2>"
+funcao "###" = "<h3>"
+funcao "####" = "<h4>"
+funcao "#####" = "<h5>"
+funcao "######" = "<h6>"
+-- Texto puro
+funcao word = word
+
+joinWordsList [] = " "
+joinWordsList list =
+              ((head list)++ " ")++ joinWordsList (tail list)
 
 
-mdHtmlParser line = do
-
-        -- let a = show line
-        let plited = splitOn " " line
-
-        return plited
-
---        let first = head head plited
-
---        let  n_word = if first == "##"
---                then "<html>"
---                else first
-
---        print n_word
+escreveArq txt = do
+              arq <- openFile "out.html" WriteMode
+              hPutStrLn arq (txt)
+              hClose arq;
