618954ffa24f32a3112bdb01a67195c96cc78923
Ian Rocha <ianrocha.df@gmail.com>
2019-10-13 14:35:28 -0300

    Adicionando comentarios para se basear

--- Prolog/main.pl
+++ Prolog/main.pl
@@ -11,3 +11,24 @@ father(pai, filho).
 mother(avóPaterna, pai).
 mother(avóMaterna, mae).
 mother(mae, filho).
+
+# Okay, we need to transfer the rules of real life to the algorithm.
+# Like an example, if your father is an Aa+ and your mother is O+
+# you like a son would have the equal amount of probability to be an
+# 50% Aa or 50% O, and 50% chance to be positive or 50% to be negative.
+
+# This is the Table:
+# XxX | Ai | Bi | ii |
+# --------------------
+#  Ai | AA | AB | Ai |
+#  Bi | AB | BB | Bi |
+#  ii | Ai | Bi | ii |
+# --------------------
+# PS: ii = O
+
+# And for defining if you're positive or negative, this is the table:
+# XxX |  +  |  -  |
+# -----------------
+#  +  | +/- | +/- |
+#  -  | +/- |  -  |
+# -----------------
